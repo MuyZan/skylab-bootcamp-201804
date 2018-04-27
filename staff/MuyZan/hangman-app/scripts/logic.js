d3c178f5f293dabd53949d4bde827f6f5fb71ca1
MuyZan <zetadezan@gmail.com>
2018-04-27 09:37:02 +0200

    update hangman app

--- staff/MuyZan/hangman-app/scripts/logic.js
+++ staff/MuyZan/hangman-app/scripts/logic.js
@@ -1,95 +1,120 @@
-'use strict'
-
-
-
-
+"use strict";
 
 class Hangman {
-    constructor(wordToGuess, numAttempts){
+  constructor(wordToGuess, numAttempts) {
+    if (!(typeof wordToGuess === "string")) {
+      throw Error("invalid word " + wordToGuess);
+    }
 
-        if(!(typeof wordToGuess === "string")){
-            throw Error ('invalid input word');
+    (this.word = wordToGuess),
+      (this.wordArray = this.word.split("")),
+      (this.count = numAttempts || 10),
+      this.matched,
+      // this.guess = new Array(this.wordArray.length).fill('_')
+      this.guess = this.fun(this.wordArray)
+  }
+
+  fun(word) {
+    let guess = word;
+    for (let i = 0; i < guess.length; i++) {
+      guess[i] = "_";
+    }
+    return guess;
+  }
+
+  try(letter) {
+    if (!(typeof letter === "string")) {
+      throw Error("invalid letter or word " + letter);
+    }
+
+    
+    if (letter.length > 1) {
+      let tryWord = this.word;
+      if (tryWord === letter) {
+        this.matching(letter);
+        return true;
+      } else {
+        this.count -= 1;
+        return false;
+      }
+  
+    } else {
+
+      if (this.word.includes(letter)) {
+        this.matching(letter);
+        return true;
+      } else {
+        this.count -= 1;
+        return false;
+      }
+ 
+    }
+  }
+
+  matching(letter) {
+      
+    if (letter.length > 1) {
+      this.matched = letter.length;
+      this.guess = this.word.split("");
+    } else {
+
+      for (let i = 0; i < this.wordArray.length; i++) {
+        if (this.wordArray[i] === letter) {
+          this.guess[i] = letter;
+          this.matched++;
         }
-        this.word = wordToGuess.split(""),
-        this.count = numAttempts || 10
+      }
+    }
+  }
 
+  attempts() {
+    return this.count;
+  }
+
+  guessed() {
+    return this.guess;
+  }
+
+
+  status(){
+  
+    let length = this.wordArray.length;
+    if(this.count === 0){
+        Hangman.CONTINUE = false;
+        this.endGame();
+       return  Hangman.LOSE         
+    } else if(length === this.matched){
+        Hangman.CONTINUE = false;
+        this.endGame();
+        return Hangman.WIN;
+    } else {
+       return Hangman.CONTINUE;
     } 
 }
 
-  /*      
-        
-        this.matched,
-        this.letter = "";
-        this.guess(this.word);
-        this.try(letter),
-        this.guessed(),
-        this.attempts(numAttempts),
-        this.status(),
-        this.matched(),
-        this.endGame(),
-        this.CONTINUE = true;
-        this.LOSE = false;
-        this.WIN = false;
-
-    }
-
-    guess(word){
-        let guess = this.word;
-        for(let i=0; i < guess.length; i++){
-            guess[i] = "_";
-        }
-    }
-
-    try(letter){
-            if(this.word.includes("letter")){
-                this.matched(letter);
-                return true;
-            } else{
-                this.count -= 1;
-                return false;
-            } 
-            status();
-    }
+endGame(){
+    this.WIN ?  "YOU WIN" : "YOU LOSE";
+}
+}
 
 
-    matched(letter){
-        for(let i =0; i < this.word.length; i++){
-            if(this.word[i] === letter){
-                this.guess[i] = letter;
-                matched++;
-            }
-        }
-    }
+Hangman.CONTINUE = true;
+Hangman.LOSE = false;
+Hangman.WIN = false;
 
-    guessed(){
-        return this.guess;
-    }
 
-    attempts(){  
-        return this.count;
-    }
 
-    endGame(){
-        this.WIN ?  "YOU WIN" : "YOU LOSE";
-    }
 
-    status(){
-        let length = this.word.length;
-        if(this.count === 0){
-            Hangman.CONTINUE = false;
-            this.endGame();
-           return  Hangman.LOSE         
-        } else if(length === matched){
-            Hangman.CONTINUE = false;
-            this.endGame();
-            return Hangman.WIN;
-        } else {
-           return Hangman.CONTINUE;
-        }  */
+
+    
+
+ 
 
  
 
 
 
+  
 
- 
+  
+
